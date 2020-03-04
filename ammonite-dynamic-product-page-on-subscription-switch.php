c875826734359a7a4c77e2fd2505d79ea35f8993
Daniel Ellis <danielshawellis@gmail.com>
2020-03-04 09:36:40 -0600

    set up product page content filter and add documentation

--- ammonite-dynamic-product-page-on-subscription-switch.php
+++ ammonite-dynamic-product-page-on-subscription-switch.php
@@ -1,10 +1,19 @@
 <?php
 /**
  * Plugin Name:       Ammonite Dynamic Product Page On Subscription Switch
- * Description:       Adds two enclosing shortcodes ([amdv-sub-switch-dynamic-product-normal][/...] and [amdv-sub-switch-dynamic-product-switch][/...]) to dynamically display different content for normal product page views and subscription switches.
- * Version:           1.0.0
+ * Description:       Dynamically displays a different template for subscription switches on "Hero's Choice Tincture" product page. This plugin depends on the Ammonite Custom plugin.
  * Author:            Daniel Ellis
  * Author URI:        https://danielellisdevelopment.com/
+ */
+
+/**
+ * DOCUMENTATION:
+ * -Dependencies:
+ *  -This plugin depends upon the function "amdv_product_page_subscription_submit()" within the file ammonite-custom.js within the Ammonite Custom plugin.
+ * -Functionality:
+ *  -This plugin checks if the page is a single product with ID of 2978 (Hero's Choice Tincture) and if the URL contains the string "switch-subscription".
+ *  -If both of the above conditions are true, it replaces the Hero's Choice Tincture product page with a new template.
+ *  -This template contains a UI that depends on a script in the Ammonite Custom plugin to interact with a hidden default WooCommerce Add To Cart interface.
  */
 
 /*
@@ -21,6 +30,7 @@ if ( !class_exists( 'Ammonite_Dynamic_Product_Page_On_Subscription_Switch' ) ) {
   class Ammonite_Dynamic_Product_Page_On_Subscription_Switch {
     // Add class methods here
     public static function dynamically_load_content( $content ) {
+      // Change targeted product ID below
       if ( is_single( 2978 ) && is_product() && strpos( $_SERVER['REQUEST_URI'], 'switch-subscription' ) ) {
         include( 'templates/switch-page-template.php' );
       } else {
--- templates/switch-page-template.php
+++ templates/switch-page-template.php
@@ -1,1 +1,66 @@
-<h1>Test</h1>
+<style scoped media="screen">
+  .amdv-subscription-switch-ui-container {
+    padding-top: 30px;
+  }
+
+  .amdv-subscription-switch-heading {
+    text-align: center;
+    font-size: 30px;
+    line-height: 45px;
+  }
+
+  @media screen and (min-width: 768px) {
+    .amdv-subscription-switch-ui-container {
+      width: 66.67%;
+      margin: auto;
+    }
+  }
+</style>
+
+<div class="amdv-subscription-switch-ui-container">
+  <div class="woocommerce-notices-wrapper">
+    <?php wc_print_notices(); ?>
+  </div>
+  <div class="amdv-product-page-hidden-add-to-cart-form">
+      <?php woocommerce_template_single_add_to_cart(); ?>
+  </div>
+  <h2 class="amdv-subscription-switch-heading">You Are Only Changing Your Subscription and Not Making a New Order</h2>
+  <div class="amdv-product-page-subscription-or-single-container">
+    <div class="amdv-product-page-visible" id="amdv-product-page-subscription-container">
+      <h5 class="amdv-product-page-how-much-heading">How Many:</h5>
+      <form class="" action="index.html" method="post">
+  		<button id="amdv-product-how-much-btn-one" class="amdv-product-page-button-clicked amdv-product-page-button-how-much" type="button" name="how-much" value="one-bottle" onclick="amdv_product_page_how_much_btn_click(this.value)">1 Bottle</button>
+  		<button id="amdv-product-how-much-btn-two" class="amdv-product-page-button-unclicked amdv-product-page-button-how-much" type="button" name="how-much" value="two-bottles" onclick="amdv_product_page_how_much_btn_click(this.value)">2 Bottles</button>
+  		<button id="amdv-product-how-much-btn-three" class="amdv-product-page-button-unclicked amdv-product-page-button-how-much" type="button" name="how-much" value="three-bottles" onclick="amdv_product_page_how_much_btn_click(this.value)">3 Bottles</button>
+      </form>
+      <h5 class="amdv-product-page-how-often-heading">How Often:</h5>
+      <form class="" action="index.html" method="post">
+  		<button id="amdv-product-how-often-btn-eight" class="amdv-product-page-button-unclicked amdv-product-page-button-how-often" type="button" name="how-often" value="eight-weeks" onclick="amdv_product_page_how_often_btn_click(this.value)">8 Weeks</button>
+  		<button id="amdv-product-how-often-btn-six" class="amdv-product-page-button-clicked amdv-product-page-button-how-often" type="button" name="how-often" value="six-weeks" onclick="amdv_product_page_how_often_btn_click(this.value)">6 Weeks</button>
+  		<button id="amdv-product-how-often-btn-four" class="amdv-product-page-button-unclicked amdv-product-page-button-how-often" type="button" name="how-often" value="four-weeks" onclick="amdv_product_page_how_often_btn_click(this.value)">4 Weeks</button>
+  		<button id="amdv-product-how-often-btn-three" class="amdv-product-page-button-unclicked amdv-product-page-button-how-often" type="button" name="how-often" value="three-weeks" onclick="amdv_product_page_how_often_btn_click(this.value)">3 Weeks</button>
+      </form>
+      <input class="amdv-product-page-join-now-button" type="button" name="join-now" value="join now" onclick="amdv_product_page_subscription_submit()">
+    </div>
+    <div id="amdv-product-page-not-sure-separator-text" class="row amdv-product-page-separator-text-row">
+      <div class="col-xs-12">
+        <p class="amdv-product-page-text-divider">
+          <span>Not Sure?</span>
+        </p>
+      </div>
+    </div>
+    <div id="amdv-product-page-not-sure-dropdown" class="row">
+      <div class="col-xs-12">
+        <h5>Choose Based On Your Dosing</h5>
+        <ul>
+          <li><strong>¼</strong> dropper 2x / day <strong>1 bottle</strong> lasts <strong>8 weeks</strong></li>
+          <li><strong>¼</strong> dropper 3x / day <strong>1 bottle</strong> lasts <strong>6 weeks</strong></li>
+          <li><strong>½</strong> dropper 2x / day <strong>1 bottle</strong> lasts <strong>4 weeks</strong></li>
+          <li><strong>½</strong> dropper 3x / day <strong>1 bottle</strong> lasts <strong>3 weeks</strong></li>
+          <li><strong>1</strong> dropper 2x / day <strong>2 bottles</strong> lasts <strong>4 weeks</strong></li>
+          <li><strong>1</strong> dropper 3x / day <strong>3 bottles</strong> lasts <strong>4 weeks</strong></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
