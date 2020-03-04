90a1ab0259596502fcbbe3bdb9e0bd36d6c760a9
Daniel Ellis <danielshawellis@gmail.com>
2020-03-03 21:50:41 -0600

    add filter to incled a specific template for main product on switch

--- ammonite-dynamic-product-page-on-subscription-switch.php
+++ ammonite-dynamic-product-page-on-subscription-switch.php
@@ -20,30 +20,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( !class_exists( 'Ammonite_Dynamic_Product_Page_On_Subscription_Switch' ) ) {
   class Ammonite_Dynamic_Product_Page_On_Subscription_Switch {
     // Add class methods here
-    public static function add_normal_case_shortcode() {
-      // Use [amdv-sub-switch-dynamic-product-normal][/...] to call this shortcode
-      add_shortcode( 'amdv-sub-switch-dynamic-product-normal', array( 'Ammonite_Dynamic_Product_Page_On_Subscription_Switch', 'normal_case_shortcode_callback' ) );
-    }
-
-    public static function normal_case_shortcode_callback( $atts , $content = null ) {
-      if ( !strpos( $_SERVER['REQUEST_URI'], 'switch-subscription' ) ) {
-        return $content;
-      }
-    }
-
-    public static function add_switch_case_shortcode() {
-      // Use [amdv-sub-switch-dynamic-product-switch][/...] to call this shortcode
-      add_shortcode( 'amdv-sub-switch-dynamic-product-switch', array( 'Ammonite_Dynamic_Product_Page_On_Subscription_Switch', 'switch_case_shortcode_callback' ) );
-    }
-
-    public static function switch_case_shortcode_callback( $atts , $content = null ) {
-      if ( strpos( $_SERVER['REQUEST_URI'], 'switch-subscription' ) ) {
+    public static function dynamically_load_content( $content ) {
+      if ( is_single( 2978 ) && is_product() && strpos( $_SERVER['REQUEST_URI'], 'switch-subscription' ) ) {
+        include( 'templates/switch-page-template.php' );
+      } else {
         return $content;
       }
     }
   }
 
   // Call methods on load here
-  Ammonite_Dynamic_Product_Page_On_Subscription_Switch::add_normal_case_shortcode();
-  Ammonite_Dynamic_Product_Page_On_Subscription_Switch::add_switch_case_shortcode();
+  add_filter( 'the_content', array( 'Ammonite_Dynamic_Product_Page_On_Subscription_Switch', 'dynamically_load_content' ) );
 }
